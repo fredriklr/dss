5be157bb4e3d6fa39a0f0b531161c3fa6e21975e
Niklas Bae Pedersen <Niklas Bae Pedersen>
2016-02-03 16:29:22 +0100

    Test

--- src/dss/main.java
+++ src/dss/main.java
@@ -4,12 +4,15 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		blablabla balletiss
+
 		
 		
-		fitte slekk
 		
-		nikki
+		
+		
+		
+		
+		
 		
 	}
 
